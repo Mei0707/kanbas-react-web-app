135a8c67f4ce0d33b96620bb0e063c7811e6204d
Qiaowen Mei <meiqiaowen@meiqiaoendeiMac.attlocal.net>
2024-11-04 21:57:16 -0800

    update assignments

--- src/Kanbas/Courses/Assignments/AssignmentButton.tsx
+++ src/Kanbas/Courses/Assignments/AssignmentButton.tsx
@@ -1,9 +1,12 @@
-import GreenCheckmark from "../Modules/GreenCheckmark"
-
-const AssignmentButton = () => {
+import { IoEllipsisVertical } from "react-icons/io5";
+import {BsPlus} from "react-icons/bs"
+import "./styles.css"
+export default function AssignmentsButtons() {
   return (
-    <GreenCheckmark />
+    <div>
+      <span className="col-2 p-2 fs-6 border border-1 border-secondary custom-border-radius">40% of Total</span>
+      <BsPlus className="fs-4"/>
+      <IoEllipsisVertical className="fs-4" />
+    </div> 
   )
 }
-
-export default AssignmentButton
--- src/Kanbas/Courses/Assignments/index.tsx
+++ src/Kanbas/Courses/Assignments/index.tsx
@@ -1,116 +1,70 @@
-import { useParams } from "react-router";
+import React, { useEffect } from 'react';
+import { FaPenSquare } from "react-icons/fa";
 import { BsGripVertical } from "react-icons/bs";
-import { IoEllipsisVertical } from "react-icons/io5";
-import { FaPlus } from "react-icons/fa6";
-import LessonControlButtons from "../Modules/LessonControlButtons";
-import * as db from "../../Database";
-import { MdAssignmentAdd } from "react-icons/md";
-import { FaSearch } from "react-icons/fa";
+import AssignmentsControls from "./AssignmentsControls";
+import AssignmentHeader from "./AssignmentHeader";
+import LessonControlButtons from "./LessonControlButtons";
+import { useParams, Link, useNavigate } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { deleteAssignment } from "./reducer";
 
+interface Assignment {
+  _id: string;
+  title: string;
+  course: string;
+  description: string;
+  points: number;
+  dueDate: string;
+  availableDate: string;
+  availableUntil?: string;
+  editing?: boolean;
+}
 export default function Assignments() {
   const { cid } = useParams();
-  const assignments = db.assignments;
-  const filteredAssinments = assignments.filter(
-    (assignment) => assignment.course === cid
-  );
+  const assignments = useSelector((state: any) => state.assignments.assignments);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const courseAssignments = assignments.filter((assignment: Assignment) => assignment.course === cid);
+
+  useEffect(() => {
+    console.log('Course Assignments:', courseAssignments);
+  }, [assignments, cid]);
+
+  const handleAddAssignment = () => {
+    navigate(`/Kanbas/Courses/${cid}/Assignments/Editor`);
+  };
+
   return (
     <div id="wd-assignments" className="container mt-4">
-      <div className="d-flex align-items-center justify-content-start pb-1 mb-3 text-nowrap">
-        <div className="input-group me-5">
-          <span className="input-group-text bg-white border-end-0">
-            <FaSearch />
-          </span>
-          <input
-            type="text"
-            id="wd-search-assignment"
-            className="form-control border-start-0"
-            placeholder="Search..."
-          />
-        </div>
-        <div className="d-flex  mb-3">
-          <button id="wd-add-assignment-group" className="btn btn-lg btn-secondary me-1 float-end d-flex align-items-center">
-            <FaPlus className="me-1" />
-            <span>Group</span>
-          </button>
-          <button id="wd-add-assignment" className="btn btn-lg btn-danger me-1 float-end">
-            <FaPlus className="me-1" />
-            <span>Assignment</span>
-          </button>
-        </div>
-      </div><br />
-      <div className="d-title d-flex justify-content-between align-items-center p-3 ps-2 bg-light">
-        <div className="d-flex">
-          <BsGripVertical className="me-2 fs-3" />
-          <a id="wd-assignments-title" className="h3 text-decoration-none">
-            <strong>ASSIGNMENTS</strong>
-          </a>
-        </div>
-        <div>
-          <span className="col-2 p-2 fs-6 border border-1 border-secondary custom-border-radius">40% of Total</span>
-          <FaPlus className="fs-4" />
-          <IoEllipsisVertical className="fs-4" />
-        </div>
-      </div>
+      <AssignmentsControls onAddAssignment={handleAddAssignment} /><br />
+      <AssignmentHeader />
       <ul id="wd-assignment-list" className="list-group rounded-0" style={{ borderLeft: '4px solid green' }}>
-        {filteredAssinments.map((assignment) => (
-        <li key={assignment._id} className="wd-assignment-list-item list-group-item d-flex justify-content-between p-0 fs-5 border-gray">
-          <div className="col-1 d-flex align-items-center justify-content-start">
-            <a className="wd-assignment-link d-flex align-items-center p-2" href={`#/Kanbas/Courses/${cid}/Assignments/${assignment._id}`}>
-              <BsGripVertical className="me-2 fs-3 " style={{ color: 'black' }} />
-              <MdAssignmentAdd className="fs-3 " style={{ color: 'green' }} />
-            </a>
-          </div>
-          <div className="col-7 pt-3 pb-3">
-            <div><strong>{assignment.title}</strong></div>
-            <div>
-              <span className="text-danger">Multiple Modules</span> | <strong>Not available until</strong> May 6 at 12:00am |
-              <strong> Due</strong> May 13 at 11:59pm | 100 pts
+        {courseAssignments.map((assignment : Assignment) => (
+          <li key={assignment._id} className="wd-assignment-list-item list-group-item d-flex justify-content-between p-0 fs-5 border-gray">
+            <div className="col-1 d-flex align-items-center justify-content-start">
+              <Link className="wd-assignment-link d-flex align-items-center p-2" to={`${assignment._id}`}>
+                <BsGripVertical className="me-2 fs-3 " style={{ color: 'black' }}/>
+                <FaPenSquare className="fs-3 " style={{ color: 'green' }} />
+              </Link>
             </div>
-          </div>
-          <div className="col-3 d-flex align-items-center justify-content-end p-3">
-            <LessonControlButtons />
-          </div>
-        </li>
+            <div className="col-7 pt-3 pb-3">
+              <div><strong>{assignment.title}</strong></div>
+              <div>
+                <span className="text-danger">Multiple Modules</span> | <strong>Not available until</strong> {assignment.availableDate} |
+                <strong> Due</strong> {assignment.dueDate} | {assignment.points} pts
+              </div>
+            </div>
+            <div className="col-3 d-flex align-items-center justify-content-end p-3">
+              <LessonControlButtons assignmentTitle={assignment.title} assignmentId={assignment._id} />
+            </div>
+          </li>
         ))}
-
-        {/* <li className="wd-assignment-list-item list-group-item d-flex justify-content-between p-0 fs-5 border-gray">
-          <div className="col-1 d-flex align-items-center justify-content-start me-2">
-            <a className="wd-assignment-link d-flex align-items-center p-2" href="#/Kanbas/Courses/1234/Assignments/124">
-              <BsGripVertical className="me-2 fs-3" style={{ color: 'black' }} />
-              <MdAssignmentAdd className="fs-3" style={{ color: 'green' }} />
-            </a>
-          </div>
-          <div className="col-7 pt-3 pb-3">
-            <div><strong>A2</strong></div>
-            <div>
-              <span className="text-danger">Multiple Modules</span> | <strong>Not available until</strong> May 13 at 12:00am |
-              <strong> Due</strong> May 20 at 11:59pm | 100 pts
-            </div>
-          </div>
-          <div className="col-3 d-flex align-items-center justify-content-end p-3">
-            <LessonControlButtons />
-          </div>
-        </li>
-        <li className="wd-assignment-list-item list-group-item d-flex justify-content-between p-0 fs-5 border-gray">
-          <div className="col-1 d-flex align-items-center justify-content-start me-2">
-            <a className="wd-assignment-link d-flex align-items-center p-2" href="#/Kanbas/Courses/1234/Assignments/125">
-              <BsGripVertical className="me-2 fs-3" style={{ color: 'black' }} />
-              <MdAssignmentAdd className="fs-3" style={{ color: 'green' }} />
-            </a>
-          </div>
-          <div className="col-7 pt-3 pb-3">
-            <div><strong>A3</strong></div>
-            <div>
-              <span className="text-danger">Multiple Modules</span> | <strong>Not available until</strong> May 20 at 12:00am |
-              <strong> Due</strong> May 27 at 11:59pm | 100 pts
-            </div>
-          </div>
-          <div className="col-3 d-flex align-items-center justify-content-end p-3">
-            <LessonControlButtons />
-          </div>
-        </li> */}
-
       </ul>
     </div>
   );
 }
+
+
+
+
