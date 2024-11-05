694687a127ff9dea7a38781a85090891b9f5dced
Qiaowen Mei <meiqiaowen@meiqiaoendeiMac.attlocal.net>
2024-11-04 21:50:43 -0800

    update account

--- src/Kanbas/Account/Navigation.tsx
+++ src/Kanbas/Account/Navigation.tsx
@@ -1,9 +1,25 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import { useSelector } from "react-redux";
+
 export default function AccountNavigation() {
+  const { currentUser } = useSelector((state: any) => state.accountReducer);
+  const links = currentUser ? ["Profile"] : ["Signin", "Signup"];
+  const { pathname } = useLocation();
   return (
     <div id="wd-account-navigation">
-      <Link to={`/Kanbas/Account/Signin`}  > Signin  </Link> <br/>
+      {links.map((link) => (
+        <div key={link}>
+          <Link
+            to={`/Kanbas/Account/${link}`}
+            className={pathname.includes(link) ? "active" : ""}
+          >
+            {link}
+          </Link>
+          <br />
+        </div>
+      ))}
+      {/* <Link to={`/Kanbas/Account/Signin`}  > Signin  </Link> <br/>
       <Link to={`/Kanbas/Account/Signup`}  > Signup  </Link> <br/>
-      <Link to={`/Kanbas/Account/Profile`} > Profile </Link> <br/>
+      <Link to={`/Kanbas/Account/Profile`} > Profile </Link> <br/> */}
     </div>
 );}
